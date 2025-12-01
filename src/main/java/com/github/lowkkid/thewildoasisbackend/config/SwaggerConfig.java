30b4214c1c294f66c39e0de869408df69f3420e6
lowkkid <lowkkidvstheworld@gmail.com>
2025-12-01 18:24:57 +0300

    fix swagger

--- src/main/java/com/github/lowkkid/thewildoasisbackend/config/SwaggerConfig.java
+++ src/main/java/com/github/lowkkid/thewildoasisbackend/config/SwaggerConfig.java
@@ -2,19 +2,40 @@ package com.github.lowkkid.thewildoasisbackend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springdoc.webmvc.api.OpenApiResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    @Profile("prod")
+    public OpenAPI prodOpenAPI() {
+        return createOpenAPI(List.of(
+                new Server().url("https://the-wild-oasis-backend.lowkkid.dev").description("Production server")));
+    }
+
+    @Bean
+    @Profile("local")
+    public OpenAPI localOpenAPI() {
+        return createOpenAPI(List.of(
+                new Server().url("http://localhost:8080/api/v1/the-wild-oasis").description("Local server")));
+    }
+
+
+    private OpenAPI createOpenAPI(List<Server> servers) {
         return new OpenAPI()
                 .info(new Info()
                         .title("The Wild Oasis Backend API")
                         .version("1.0")
-                        .description("API documentation for The Wild Oasis Backed"));
+                        .description("API documentation for The Wild Oasis Backend"))
+                .servers(servers);
     }
 }
