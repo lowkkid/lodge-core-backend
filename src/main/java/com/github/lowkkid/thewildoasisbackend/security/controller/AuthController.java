bbd87f3ed153fde6b65b8a3ba0853545cf361a36
lowkkid <lowkkidvstheworld@gmail.com>
2025-12-24 15:53:42 +0300

    logout endpoint

--- src/main/java/com/github/lowkkid/thewildoasisbackend/security/controller/AuthController.java
+++ src/main/java/com/github/lowkkid/thewildoasisbackend/security/controller/AuthController.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +25,21 @@ public class AuthController {
             @RequestBody @Valid UsernameAndPassword usernameAndPassword, HttpServletResponse response) {
         var tokensResponse = authService.login(usernameAndPassword, response);
         return ResponseEntity.ok(tokensResponse);
+    }
+
+    @Operation(summary = "Sign out current user. Endpoint clears cookie with refresh token")
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        var clearRefreshTokenCookie = ResponseCookie.from("refreshToken") // `from` without value creates empty cookie
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .partitioned(true)
+                .path("/")
+                .maxAge(0)
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, clearRefreshTokenCookie.toString());
+        return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "Get new pair of tokens (access token inside response body and refresh token in cookies) " +
